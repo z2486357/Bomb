3268f84d906f4e443fa34d8a6dde61d99f4e373d
z2486357 <z2486357@gmail.com>
2018-10-16 18:28:54 -0700

    update

--- app/src/main/java/com/example/aloha/bomb/MainActivity.java
+++ app/src/main/java/com/example/aloha/bomb/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     Button buttonOnClick;
     TextView textView;
     CheckBox checkBox;
+    int clicktime=0;
     Boolean finish=Boolean.FALSE;
     int[] space=new int[100]; //to decide if it is a bomb
     @Override
@@ -22,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //random for bombs
         for (int i=0;i<100;i++){
-            if (Math.random()>=0.33){
+            if (Math.random()>=0.33){//random if it is a bomb for the opportunity of 1/3 is bombs (you really need some luck to win the game XDD)
                 space[i]=0;
             }else {
                 space[i]=1;
@@ -35,17 +36,27 @@ public class MainActivity extends AppCompatActivity {
         int numberchoose;
         String[] splitid;
         String idget;
-        if(finish==Boolean.FALSE){
-            buttonOnClick=(Button)findViewById(view.getId()); //find out which button is pressed
+        buttonOnClick=(Button)findViewById(view.getId()); //find out which button is pressed
+        if(finish==Boolean.FALSE && (buttonOnClick.getText().toString().equals("X") || buttonOnClick.getText().toString().equals(""))){
+            //it will only work if the game is not finish and when you are correcting an "X" or you have not press it yet
             idget=getResources().getResourceEntryName(view.getId()); //get the id of the button
             splitid = idget.split("n");//split the id by "n"
             numberchoose=Integer.parseInt(splitid[1])-1;//the [1] is the number of button, -1 for starting form 0
             checkBox=(CheckBox)findViewById(R.id.checkBox);
             if(checkBox.isChecked()){
+                if (space[numberchoose]==1 && buttonOnClick.getText()!="X") {//this means that you find a bomb correctly
+                    clicktime += 1;//get one point
+                }
                 buttonOnClick.setText("X");//sign X if you think it is a bomb
             }else{
                 if(space[numberchoose]==1){
-                    buttonOnClick.setText("*");//if it is a bomb, explode
+                    //if it is a bomb, explode
+                    for(int i=0;i<100;i++){//show all bombs
+                        if(space[i]==1){
+                            buttonOnClick=(Button)findViewById(getResources().getIdentifier("Button" + (i+1),"id",getPackageName()));
+                            buttonOnClick.setText("*");
+                        }
+                    }
                     textView=(TextView)findViewById(R.id.textView);
                     textView.setText("Game Over");
                     finish=Boolean.TRUE;
@@ -75,9 +86,14 @@ public class MainActivity extends AppCompatActivity {
                         bombnumber+=space[numberchoose+11];//find if there is a bomb down-right
                     }
                     buttonOnClick.setText(String.valueOf(bombnumber));//show the bombs number next to it
+                    clicktime+=1;//get one point for correctly find there is no bombs here
                 }
             }
-
+        }
+        if (clicktime==100){//you win
+            textView=(TextView)findViewById(R.id.textView);
+            textView.setText("You Win!!");
+            finish=Boolean.TRUE;
         }
 
         //textView=(TextView)findViewById(R.id.textView);
@@ -92,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
         finish=Boolean.FALSE;
         textView=(TextView)findViewById(R.id.textView);
         textView.setText("");
+        clicktime=0;
         checkBox.setChecked(false);
         //random for bombs
         for (int i=0;i<100;i++){
